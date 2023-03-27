63c373e3f808f02b324f96ff11bc93f81eb760c6
Andrew <a.oleyn1kov@outlook.com>
2023-03-28 01:58:50 +0300

    utils

--- CataHornSolver/Utils2.fs
+++ CataHornSolver/Utils2.fs
@@ -7,6 +7,31 @@ open Z3Interpreter.AST
 
 let flip f a b = f b a
 
+let balancedBracket (str: string) =
+  let rec helper depth acc =
+    function
+    | _ when depth = 0 -> Some acc
+    | h :: tl ->
+      let inc =
+        match h with
+        | '(' -> 1
+        | ')' -> -1
+        | _ -> 0
+
+      helper (depth + inc) (h :: acc) tl
+    | _ -> None
+
+  str.Substring 1
+  |> Seq.toList
+  |> helper 1 []
+  |> function
+    | Some cs ->
+      List.fold (fun acc c -> sprintf "%c%s" c acc) "" cs
+      |> (fun str -> sprintf "(%s" str)
+      |> Some
+    | None -> None
+
+
 
 let varsIdxs (str: string) =
   let var = Regex "a![0-9]+"
