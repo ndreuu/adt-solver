bf84e21b8463944524d2e320aa7b1e911de3898e
Andrew <a.oleyn1kov@outlook.com>
2023-07-05 18:56:37 +0300

    CI

--- CataHornSolver/Program.fs
+++ CataHornSolver/Program.fs
@@ -7,39 +7,39 @@ open ProofBased.Solver
 module Program =
 
   [<EntryPoint>]
-  let main _ = 
+  // let main _ = 
       // let path = Path.Join(TestContext.CurrentContext.TestDirectory, @"Tests/Source/TIP-no-NAT")
-      let path = @"./Tests/Source/TIP-no-NAT"
-      printfn $"{Directory.Exists(path)}"
-      let dir = DirectoryInfo(path);
-      let files = dir.GetFiles("*.smt2"); 
-      for file in files do
-        let v, st, curDuration = run file.DirectoryName None None
-        let content = Utils.join "\n" (List.map (fun (n, t) -> $"{n} {t}") st)  
-        File.WriteAllText("./out.txt", $"{v}\n{curDuration}\n{content}")
-      0
+      // let path = @"./Tests/Source/TIP-no-NAT"
+      // printfn $"{Directory.Exists(path)}"
+      // let dir = DirectoryInfo(path);
+      // let files = dir.GetFiles("*.smt2"); 
+      // for file in files do
+      //   let v, st, curDuration = run file.DirectoryName None None
+      //   let content = Utils.join "\n" (List.map (fun (n, t) -> $"{n} {t}") st)  
+      //   File.WriteAllText("./out.txt", $"{v}\n{curDuration}\n{content}")
+      // 0
 
-  // let main = function
-  //   | [| path; dbgPath; timeLimit |] ->
-  //     let d = Path.Join [| dbgPath; "dbg" |]
-  //     if Directory.Exists d then Directory.Delete (d, true)
-  //     Directory.CreateDirectory d |> ignore
-  //     let v, st, curDuration = run path (Some d) (Some timeLimit)
-  //     printfn $"{v}"
-  //     printfn $"{curDuration}"
-  //     for s in st do printfn $"{s}"
-  //     0
-  //   | [| path; dbgPath |] ->
-  //     let d = Path.Join [| dbgPath; "dbg" |]
-  //     if Directory.Exists d then Directory.Delete (d, true)
-  //     Directory.CreateDirectory d |> ignore
-  //     let v, st, curDuration = run path (Some d) None
-  //     printfn $"{v}"
-  //     printfn $"{curDuration}"
-  //     for s in st do printfn $"{s}"
-  //     0
-  //   | [| path |] ->
-  //     printfn $"{run path None None}"
-  //     0
-  //   | _ -> 
-  //     1
+  let main = function
+    | [| path; dbgPath; timeLimit |] ->
+      let d = Path.Join [| dbgPath; "dbg" |]
+      if Directory.Exists d then Directory.Delete (d, true)
+      Directory.CreateDirectory d |> ignore
+      let v, st, curDuration = run path (Some d) (Some timeLimit)
+      printfn $"{v}"
+      printfn $"{curDuration}"
+      for s in st do printfn $"{s}"
+      0
+    | [| path; dbgPath |] ->
+      let d = Path.Join [| dbgPath; "dbg" |]
+      if Directory.Exists d then Directory.Delete (d, true)
+      Directory.CreateDirectory d |> ignore
+      let v, st, curDuration = run path (Some d) None
+      printfn $"{v}"
+      printfn $"{curDuration}"
+      for s in st do printfn $"{s}"
+      0
+    | [| path |] ->
+      printfn $"{run path None None}"
+      0
+    | _ -> 
+      1
