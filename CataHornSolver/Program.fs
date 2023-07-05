0a0accef0ac8b7ee8b2b5ded9192776f646926f1
Andrew <a.oleyn1kov@outlook.com>
2023-07-05 17:59:54 +0300

    CI

--- CataHornSolver/Program.fs
+++ CataHornSolver/Program.fs
@@ -1,17 +1,26 @@
 open System.IO
 open System.Text.RegularExpressions
 open NUnit.Framework
+open ProofBased
 open ProofBased.Solver
 
 module Program =
 
   [<EntryPoint>]
   let main _ = 
+      // let files = Path.Join(TestContext.CurrentContext.TestDirectory, "Tests/Source/adt_lia")
+      // printfn $"{Directory.Exists(files)}"
+      //
+      // let  d = DirectoryInfo(files);
+      // let a = d .GetFiles("*.smt2"); //Getting Text files
+      // printfn $"{a}"
+      //
+      
       let file = Path.Join(TestContext.CurrentContext.TestDirectory, "Tests/Source/racer.horn.smt2")
+      printfn $"{File.Exists(file)}"
       let v, st, curDuration = run file None None
-      printfn $"{v}"
-      printfn $"{curDuration}"
-      for s in st do printfn $"{s}"
+      let a = Utils.join "\n" (List.map (fun (n, t) -> $"{n} {t}") st)  
+      File.WriteAllText("./out.txt", $"{v}\n{curDuration}\n{a}")
       0
 
   // let main = function
