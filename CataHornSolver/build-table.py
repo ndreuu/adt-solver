cab5fa441f77e1666bdd9e548d4fc9b3d9ab278c
Andrew <a.oleyn1kov@outlook.com>
2023-07-14 22:17:37 +0300

    building table fix

--- CataHornSolver/build-table.py
+++ CataHornSolver/build-table.py
@@ -6,6 +6,11 @@ fstName = content[0]
 lastRes = content[-1]
 content = "".join(content)
 
+tableContent = re.sub(
+    r"\/usr\/share\/dotnet\/sdk\/6\.0\.412\/Microsoft\.Common\.CurrentVersion\.targets\(2302\,5\)\: warning MSB3270\: There was a mismatch between the processor architecture of the project being built \"MSIL\" and the processor architecture of the reference \"\/home\/runner\/\.nuget\/packages\/microsoft\.z3\/4\.11\.2\/lib\/netstandard2\.0\/Microsoft\.Z3\.dll\"\, \"AMD64\"\. This mismatch may cause runtime failures\. Please consider changing the targeted processor architecture of your project through the Configuration Manager so as to align the processor architectures between your project and references\, or take a dependency on references with a processor architecture that matches the targeted processor architecture of your project\. \[\/home\/runner\/work\/adt\-solver\/adt\-solver\/CataHornSolver\/CataHornSolver\.fsproj\]\n",
+    "",
+    content)
+
 dirty = re.findall(r"(?:.*\n.*\.smt2\n\(INIT\)SMT\.NIA)|(?:(?:.*\.smt2.*\n)*ERR .*\n.*\.smt2)", content)
 
 
